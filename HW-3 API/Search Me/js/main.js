c4a02796b20fe0c678327005b9598383a1e56594
Aimee <abalos.aimee@gmail.com>
2016-09-26 07:33:43 +0800

    add Article Search

--- HW-3 API/Search Me/js/main.js
+++ HW-3 API/Search Me/js/main.js
@@ -1,4 +1,5 @@
 var BookName = '';
+var ArticleName ='';
 var APIKey = '63e763062be34e67b1948d52e0c2a0fb';
 
 
@@ -23,12 +24,32 @@ var init = function(){
     getBookReview();
   }
   });
+
+$('#searchArticle').keypress(function (e) {
+  if (e.which == 13) {
+    event.preventDefault();
+    ArticleName = $("#inpt_search").val();
+    getArtReview();
+  }
+  });
+
 }
 
 var loadBook = function(jsonResponse){
   console.log(jsonResponse);
   if (jsonResponse.num_results == 0){
-      alert("no Result");
+      $('.bookDesc').empty();
+       $('.bookDesc').append(
+          $('<div/>')
+            .addClass("bookTitle")
+            .html("No Result")
+        );
+        $('.bookDesc').append(
+          $('<div/>')
+            .addClass("bookAuthor")
+            .html("Try \"1Q84\" or \"The Girl On The Train\"")
+        );
+      // alert("no Result");
       return;
   }
   else{
@@ -71,6 +92,38 @@ var loadBook = function(jsonResponse){
   }  
 }
 
+
+var loadArticle = function(jsonResponse){
+  
+      $('.bookDesc').empty();
+      
+
+      for (var i = 0; i <5 ; i++) {
+        // console.log(jsonResponse.results[i]);
+        var book_title = jsonResponse.response.docs[i].headline.main;
+        console.log(book_title);
+        var reviewURL = jsonResponse.response.docs[i].web_url;
+        var reviewAuthor =  jsonResponse.response.docs[i].byline.original;
+        var summary =  jsonResponse.response.docs[i].abstract;
+        var publication_dt =  jsonResponse.response.docs[i].pub_date;
+        
+       
+
+        
+
+        $('.bookDesc').append(
+          $('<div/>')
+            // .attr("id", "newDiv1")
+            .addClass("bookReview")
+            .append("<table/>")
+            .html("<tr><td>Article Title: </td><td>"+book_title+"</td><tr><td></td><td>"+reviewAuthor + "</td><tr><td>Date:</td><td> "+publication_dt + "</td><tr><td>Abstract: </td><td>"+summary + "</td><tr><td>URL: </td><td><a href="+reviewURL+">"+reviewURL+"</a></td></tr>")
+        );
+      }     
+
+}
+
+
+
 var getBookReview = function(){
   var myUrl = "http://api.nytimes.com/svc/books/v3/reviews.json?title="+ BookName;
   // console.log(myUrl);
@@ -81,6 +134,23 @@ var getBookReview = function(){
     success: function(response){
       console.log("i got the json file parsed!");
       loadBook(response);
+    }
+  });
+}
+
+var getArtReview = function(){
+  var myUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  myUrl += '?' + $.param({
+    'api-key': "63e763062be34e67b1948d52e0c2a0fb",
+    'q': ArticleName
+  });
+
+  $.ajax({
+    url:myUrl,
+    dataType: "json",
+    success: function(response){
+      console.log("i got the json file parsed!");
+      loadArticle(response);
     }
   });
 }
