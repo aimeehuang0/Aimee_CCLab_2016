d371e776b4ce27cfce5f9c3157261ebcc5483125
Aimee <abalos.aimee@gmail.com>
2016-11-26 22:46:44 -0500

    pacman

---
+++ week12/Pacman/Assets/Assets/Scripts/Pacdot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "pacman")
+        {
+            //GameObject[] pacdots = GameObject.FindGameObjectsWithTag("Pacdot");
+            Destroy(gameObject);
+        }
+    }
+}
